79fe163db85c2d73ff7c228eb8caf9a8284da5bc
mitrich <mitrich86@gmail.com>
2023-06-19 01:30:29 +0600

    add: homework task_13, task_21 and task_23

---
+++ seminar_1/task_19/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+static bool IsPalindrom(string word, bool ignoreCase = true)
+{
+    if (ignoreCase)
+    {
+        word = word.ToLowerInvariant();
+    }
+
+    for (int first = 0, last = word.Length - 1; first < last; ++first, --last)
+    {
+        if (word[first] != word[last])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Console.Write("Введите положительное пятизначное число: ");
+string number = Console.ReadLine();
+int number2 = int.Parse(number);
+if ((number2 > 9999) && (number2 < 100000))
+{
+    if (IsPalindrom(number) == true)
+        {
+            Console.Write($"Число {number} является палиндромом");
+        }
+    else 
+    Console.Write($"Число {number} НЕ является палиндромом");
+}
+else
+    Console.Write($"Введеное число не является пятизначным или положительным -> {number2}");
--- seminar_1/task_21/Program.cs
+++ seminar_1/task_21/Program.cs
@@ -1,12 +1,13 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
-// расстояние между ними в 2D пространстве.
+﻿// Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
 
-// A (3,6); B (2,1) -> 5,09 
-// A (7,-5); B (1,-1) -> 7,21
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double GetDistance(int x1, int x2, int y1, int y2)
+
+double GetDistance(int x1, int x2, int y1, int y2, int z1, int z2)
 {
-    double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
     return result;
 }
 
@@ -16,12 +17,19 @@ int x1 = int.Parse(Console.ReadLine());
 Console.Write("Введите координату Y1: ");
 int y1 = int.Parse(Console.ReadLine());
 
+Console.Write("Введите координату Z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
 Console.Write("Введите координату Х2: ");
 int x2 = int.Parse(Console.ReadLine());
 
 Console.Write("Введите координату Y2: ");
 int y2 = int.Parse(Console.ReadLine());
 
+Console.Write("Введите координату Z2: ");
+int z2 = int.Parse(Console.ReadLine());
 
-double finish = GetDistance(x1, y1, x2, y2);
-Console.Write(Math.Round(finish,2,MidpointRounding.ToNegativeInfinity));
+
+
+double finish = GetDistance(x1, x2, y1, y2, z1, z2);
+Console.Write(Math.Round(finish, 2, MidpointRounding.ToEven));
